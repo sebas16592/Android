0c6358bff502d69d73fc9aeb67124361df2a4535
sebastian <ingeniero.desarollo8@comersantander.com>
2018-04-03 16:58:55 -0500

    Intent

--- Platzigram/app/src/main/java/com/sebhernandez/platzigram/LoginActivity.java
+++ Platzigram/app/src/main/java/com/sebhernandez/platzigram/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.sebhernandez.platzigram;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.sebhernandez.platzigram.view.CreateAccountActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void goCreateAccount(View view){
+        Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
+
     }
 }
