a0421ea6eaa83f88ef76f3e7c6fd66cb809e959d
sebastian <ingeniero.desarollo8@comersantander.com>
2018-04-05 08:42:34 -0500

    tabs

--- Platzigram/app/src/main/java/com/sebhernandez/platzigram/LoginActivity.java
+++ Platzigram/app/src/main/java/com/sebhernandez/platzigram/LoginActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.sebhernandez.platzigram.view.ContainerActivity;
 import com.sebhernandez.platzigram.view.CreateAccountActivity;
 
 public class LoginActivity extends AppCompatActivity {
@@ -19,5 +22,22 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(this, CreateAccountActivity.class);
         startActivity(intent);
 
+    }
+
+    public void goLogin(View view){
+        EditText username = (EditText) findViewById(R.id.username);
+        EditText password = (EditText) findViewById(R.id.password);
+        String user=username.getText().toString();
+        String pass=password.getText().toString();
+        if (user.equals("Sebas") && pass.equals("123")){
+            Intent intent = new Intent(this, ContainerActivity.class);
+            startActivity(intent);
+        }else{
+            Toast toast1 =
+                    Toast.makeText(getApplicationContext(),
+                            "Debe ingresar el usuario correcto", Toast.LENGTH_SHORT);
+
+            toast1.show();
+        }
     }
 }
